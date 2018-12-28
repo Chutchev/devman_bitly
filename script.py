e69922ea6cc96618d2cb99b649ff6674165f303b
chutchev <kolovorot2233@gmail.com>
2018-12-28 21:22:02 +0500

    load_dotenv() помещено внутрь def main()

--- script.py
+++ script.py
@@ -1,19 +1,17 @@
 import requests
 import os
-import dotenv
+from dotenv import load_dotenv
 import argparse
-dotenv.load_dotenv()
-TOKEN = os.getenv('TOKEN')
 
 
-def check_link(url):
-    header = {"Authorization": f"Bearer {TOKEN}"}
+def check_link(url, token):
+    header = {"Authorization": f"Bearer {token}"}
     req = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}', headers=header)
     return req.ok
 
 
-def get_short_url(long_url):
-    header = {"Authorization": f"Bearer {TOKEN}"}
+def get_short_url(long_url,token):
+    header = {"Authorization": f"Bearer {token}"}
     body = {"long_url": long_url}
     post_req = requests.post("https://api-ssl.bitly.com/v4/bitlinks", headers=header, json=body)
     if not post_req.ok:
@@ -24,8 +22,8 @@ def get_short_url(long_url):
         return bitlink[1]
 
 
-def get_amount_click(short_url):
-    header = {"Authorization": f"Bearer {TOKEN}"}
+def get_amount_click(short_url, token):
+    header = {"Authorization": f"Bearer {token}"}
     params = {"unit": "day", "units": -1}
     req = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{short_url}/clicks/summary",
                             json=params, headers=header)
@@ -37,15 +35,18 @@ def get_amount_click(short_url):
 
 
 def main():
+    load_dotenv()
+    token = os.getenv('TOKEN')
     parser = argparse.ArgumentParser(description="USAGE: python script.py your_url")
     parser.add_argument("url", help="Ссылка на сокращение/Подсчет количества кликов")
     args = parser.parse_args()
-    if check_link(args.url):
-      info = get_amount_click(args.url)
+    if check_link(args.url, token):
+      info = get_amount_click(args.url, token)
     else:
-      info = get_short_url(args.url)
+      info = get_short_url(args.url, token)
     print(info)
 
 
 if __name__ == "__main__":
     main()
+
